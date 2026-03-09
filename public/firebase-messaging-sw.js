a91a288090d93ddd2872262f73fc371d93a94f49
villamanzanares <jjgg2007@gmail.com>
2026-03-09 12:39:14 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,29 +1,31 @@
-importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js");
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
   authDomain: "alerta-rosko.firebaseapp.com",
   projectId: "alerta-rosko",
-  storageBucket: "alerta-rosko.appspot.com",
+  storageBucket: "alerta-rosko.firebasestorage.app",
   messagingSenderId: "1022811358317",
   appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
-});
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[Service Worker] Background message received:', payload);
 
-  const notificationTitle = payload.notification?.title || '🚨 Alerta';
+  console.log("📩 Mensaje recibido en background:", payload);
+
+  const notificationTitle = payload.notification?.title || "🚨 Alerta";
+  
   const notificationOptions = {
-    body: payload.notification?.body || '¡Revisa la aplicación!',
-    icon: '/favicon.ico',
-    badge: '/favicon.ico',
-    vibrate: [200, 100, 200],
-    tag: 'alerta-unica',
-    renotify: true
+    body: payload.notification?.body || "Nueva alerta vecinal",
+    icon: "/favicon.ico",
+    vibrate: [200,100,200]
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+
 });
