aa3b281c1a406da093e779836a0659bebc3531cc
villamanzanares <jjgg2007@gmail.com>
2026-03-10 18:36:49 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,70 +2,54 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-
-apiKey:"AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
-authDomain:"alerta-rosko.firebaseapp.com",
-projectId:"alerta-rosko",
-messagingSenderId:"1022811358317",
-appId:"1:1022811358317:web:ce210848e7ed63d1412b64"
-
+  apiKey:"TU_API_KEY",
+  authDomain:"alerta-rosko.firebaseapp.com",
+  projectId:"alerta-rosko",
+  messagingSenderId:"1022811358317",
+  appId:"1:1022811358317:web:ce210848e7ed63d1412b64"
 });
 
 const messaging = firebase.messaging();
 
 
+/* CUANDO LLEGA UNA ALERTA */
+
 messaging.onBackgroundMessage(function(payload){
 
-const tipo = payload.data.tipo;
-const lat = payload.data.lat;
-const lng = payload.data.lng;
+  const tipo = payload.data.tipo;
+  const lat = payload.data.lat;
+  const lng = payload.data.lng;
 
-self.registration.showNotification("🚨 Villa Segura",{
+  self.registration.showNotification("🚨 Villa Segura",{
 
-body:"Alerta de "+tipo,
-icon:"/icon.png",
+    body:"Alerta de "+tipo,
+    icon:"/icon.png",
 
-data:{
-lat:lat,
-lng:lng,
-tipo:tipo
-}
+    data:{
+      lat:lat,
+      lng:lng,
+      tipo:tipo
+    }
 
-});
-
-
-self.clients.matchAll().then((clients)=>{
-
-clients.forEach((client)=>{
-
-client.postMessage({
-
-tipo:"alerta",
-lat:lat,
-lng:lng
+  });
 
 });
 
-});
 
-});
-
-});
-
+/* CUANDO TOCAN LA NOTIFICACION */
 
 self.addEventListener("notificationclick",(event)=>{
 
-event.notification.close();
+  event.notification.close();
 
-const lat = event.notification.data.lat;
-const lng = event.notification.data.lng;
+  const lat = event.notification.data.lat;
+  const lng = event.notification.data.lng;
+  const tipo = event.notification.data.tipo;
 
-const url = "https://www.google.com/maps?q="+lat+","+lng;
+  const url = "/?lat="+lat+"&lng="+lng+"&tipo="+tipo;
 
-event.waitUntil(
-
-clients.openWindow(url)
-
-);
+  event.waitUntil(
+    clients.openWindow(url)
+  );
 
 });
