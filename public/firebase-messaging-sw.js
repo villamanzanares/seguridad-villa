41a173b343e44f098d33da9d58798c908bd8f873
villamanzanares <jjgg2007@gmail.com>
2026-03-10 17:49:23 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,11 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
-  authDomain: "alerta-rosko.firebaseapp.com",
-  projectId: "alerta-rosko",
-  messagingSenderId: "1022811358317",
-  appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
+
+apiKey:"AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
+authDomain:"alerta-rosko.firebaseapp.com",
+projectId:"alerta-rosko",
+messagingSenderId:"1022811358317",
+appId:"1:1022811358317:web:ce210848e7ed63d1412b64"
+
 });
 
 const messaging = firebase.messaging();
@@ -14,34 +16,56 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload){
 
-  const tipo = payload.data.tipo;
-  const lat = payload.data.lat;
-  const lng = payload.data.lng;
+const tipo = payload.data.tipo;
+const lat = payload.data.lat;
+const lng = payload.data.lng;
 
-  const url = "https://www.google.com/maps?q=" + lat + "," + lng;
+self.registration.showNotification("🚨 Villa Segura",{
 
-  self.registration.showNotification("🚨 Villa Segura", {
+body:"Alerta de "+tipo,
+icon:"/icon.png",
 
-    body: "Alerta de " + tipo,
-    icon: "/icon.png",
-
-    data: {
-      url: url
-    }
-
-  });
+data:{
+lat:lat,
+lng:lng,
+tipo:tipo
+}
 
 });
 
 
-self.addEventListener("notificationclick", function(event){
+self.clients.matchAll().then((clients)=>{
 
-  event.notification.close();
+clients.forEach((client)=>{
 
-  const url = event.notification.data.url;
+client.postMessage({
 
-  event.waitUntil(
-    clients.openWindow(url)
-  );
+tipo:"alerta",
+lat:lat,
+lng:lng
+
+});
+
+});
+
+});
+
+});
+
+
+self.addEventListener("notificationclick",(event)=>{
+
+event.notification.close();
+
+const lat = event.notification.data.lat;
+const lng = event.notification.data.lng;
+
+const url = "https://www.google.com/maps?q="+lat+","+lng;
+
+event.waitUntil(
+
+clients.openWindow(url)
+
+);
 
 });
