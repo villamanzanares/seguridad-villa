bf49faed93031c12b579231fb85a7f38fc061c0a
villamanzanares <jjgg2007@gmail.com>
2026-03-10 19:01:41 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,55 +1,27 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey:"TU_API_KEY",
-  authDomain:"alerta-rosko.firebaseapp.com",
-  projectId:"alerta-rosko",
-  messagingSenderId:"1022811358317",
-  appId:"1:1022811358317:web:ce210848e7ed63d1412b64"
+  apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
+  authDomain: "alerta-rosko.firebaseapp.com",
+  projectId: "alerta-rosko",
+  messagingSenderId: "1022811358317",
+  appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
 });
 
 const messaging = firebase.messaging();
 
-
-/* CUANDO LLEGA UNA ALERTA */
-
-messaging.onBackgroundMessage(function(payload){
-
-  const tipo = payload.data.tipo;
-  const lat = payload.data.lat;
-  const lng = payload.data.lng;
-
-  self.registration.showNotification("🚨 Villa Segura",{
-
-    body:"Alerta de "+tipo,
-    icon:"/icon.png",
-
-    data:{
-      lat:lat,
-      lng:lng,
-      tipo:tipo
-    }
-
+// Notificaciones en background
+messaging.onBackgroundMessage(payload => {
+  const { tipo } = payload.data;
+  self.registration.showNotification(`Alerta: ${tipo}`, {
+    body: `Toca para abrir la app`,
+    icon: '/icon.png'
   });
-
 });
 
-
-/* CUANDO TOCAN LA NOTIFICACION */
-
-self.addEventListener("notificationclick",(event)=>{
-
+// Al tocar notificación
+self.addEventListener('notificationclick', event => {
   event.notification.close();
-
-  const lat = event.notification.data.lat;
-  const lng = event.notification.data.lng;
-  const tipo = event.notification.data.tipo;
-
-  const url = "/?lat="+lat+"&lng="+lng+"&tipo="+tipo;
-
-  event.waitUntil(
-    clients.openWindow(url)
-  );
-
+  event.waitUntil(clients.openWindow('/'));
 });
