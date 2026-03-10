9e2134cc557e3d10c5373439f4a659d23bb42bd1
villamanzanares <jjgg2007@gmail.com>
2026-03-10 16:57:05 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,36 +1,64 @@
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
-
 firebase.initializeApp({
-
   apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
   authDomain: "alerta-rosko.firebaseapp.com",
   projectId: "alerta-rosko",
   messagingSenderId: "1022811358317",
   appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
-
 });
-
 
 const messaging = firebase.messaging();
 
 
+/* CUANDO LLEGA LA NOTIFICACIÓN */
+
 messaging.onBackgroundMessage(function(payload) {
 
-  console.log("Notificación recibida:", payload);
+  const tipo = payload.data.tipo;
+  const lat = payload.data.lat;
+  const lng = payload.data.lng;
 
-  self.registration.showNotification(
+  const notificationTitle = "🚨 Villa Segura";
 
-    payload.notification.title,
+  const notificationOptions = {
 
-    {
-      body: payload.notification.body,
-      icon: "/icon.png",
-      vibrate: [200,100,200,100,200],
-      requireInteraction: true
-    }
+    body: "Alerta de " + tipo,
+    icon: "/icon.png",
 
+    data: {
+      lat: lat,
+      lng: lng
+    },
+
+    actions: [
+      {
+        action: "ver",
+        title: "📍 Ver ubicación"
+      }
+    ]
+
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+
+});
+
+
+/* CUANDO EL USUARIO TOCA LA NOTIFICACIÓN */
+
+self.addEventListener("notificationclick", function(event){
+
+  const lat = event.notification.data.lat;
+  const lng = event.notification.data.lng;
+
+  event.notification.close();
+
+  const url = "https://www.google.com/maps?q=" + lat + "," + lng;
+
+  event.waitUntil(
+    clients.openWindow(url)
   );
 
 });
