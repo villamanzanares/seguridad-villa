c94af61191a77f171367f65126ac1b2837f54fc7
villamanzanares <jjgg2007@gmail.com>
2026-03-19 15:21:29 -0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,18 @@
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
   authDomain: "alerta-rosko.firebaseapp.com",
   projectId: "alerta-rosko",
   messagingSenderId: "1022811358317",
   appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 
 const messaging = firebase.messaging();
 
-/* recibir notificación en segundo plano */
-
+/* BACKGROUND */
 messaging.onBackgroundMessage(function(payload) {
-
-  console.log("Notificación en background:", payload);
 
   const notificationTitle = payload.notification.title;
 
